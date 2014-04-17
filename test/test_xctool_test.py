e0be0a510e7cfc75a44477a09551bd9f9f1f827a
Michael Ford <mtford@gmail.com>
2014-04-17 15:23:34 +0100

    Run tests.

--- test/test_xctool_test.py
+++ test/test_xctool_test.py
@@ -1,52 +1,83 @@
 import unittest
-from mock import MagicMock
 
-from xctool_test import XCToolTest, XCToolError
+from mock import MagicMock
+from xctool_parser import XCToolParser
+
+from xctool_test import XCToolTest, XCToolError, BuildError, RunError
 
 
 class TestXCToolTest(unittest.TestCase):
     def test_default_path(self):
-        test = XCToolTest(None, None, None)
+        test = XCToolTest(None, None, None, None)
         self.assertEqual(test.xctool_path, XCToolTest.default_xc_tool_path)
-        test = XCToolTest(None, None, None, xctool_path='blah')
+        test = XCToolTest(None, None, None, None, xctool_path='blah')
         self.assertEqual(test.xctool_path, 'blah')
+
+    def test_default_log_paths(self):
+        test = XCToolTest(None, None, None, None)
+        self.assertEqual(test.default_run_log_stderr, test.log_path_run_stderr)
+        self.assertEqual(test.default_run_log_stdout, test.log_path_run_stdout)
+        self.assertEqual(test.default_build_log_stderr, test.log_path_build_stderr)
+        self.assertEqual(test.default_build_log_stdout, test.log_path_build_stdout)
 
 
 # noinspection PyProtectedMember
 class TestXCCoolTestOnlyParam(unittest.TestCase):
     def test_target(self):
-        test = XCToolTest(None, None, target='target')
+        test = XCToolTest(None, None, None, target='target')
         self.assertEqual('target', test._construct_only())
 
     def test_target_class(self):
-        test = XCToolTest(None, None, target='target', test_class='class')
+        test = XCToolTest(None, None, None, target='target', test_class='class')
         self.assertEqual('target:class', test._construct_only())
 
     def test_target_class_method(self):
-        test = XCToolTest(None, None, target='target', test_class='class', test_method='method')
+        test = XCToolTest(None, None, None, target='target', test_class='class', test_method='method')
         self.assertEqual('target:class/method', test._construct_only())
 
     def test_target_method_throws_error(self):
-        test = XCToolTest(None, None, target='target', test_method='method')
+        test = XCToolTest(None, None, None, target='target', test_method='method')
         self.assertRaises(XCToolError, test._construct_only)
 
 
 # noinspection PyProtectedMember
 class XCToolTestBuild(unittest.TestCase):
-
     def test_execute(self):
-        test = XCToolTest(None, None, None)
+        test = XCToolTest(None, None, None, None)
         stdout, stderr, return_code = test._execute("echo 'hello stderr' >&2; echo 'hello stdout'; False")
         self.assertIn('hello stderr', stderr)
         self.assertIn('hello stdout', stdout)
         self.assertEqual(1, return_code)
 
     def test_build_success(self):
-        test = XCToolTest(None, None, None)
+        test = XCToolTest(None, None, None, None)
         test._execute = MagicMock(return_value=('', '', 0))
-        self.assertTrue(test._build())
+        test._build()
 
     def test_build_failure(self):
-        test = XCToolTest(None, None, None)
+        test = XCToolTest(None, None, None, None)
         test._execute = MagicMock(return_value=('', '', 1))
-        self.assertFalse(test._build())
+        self.assertRaises(BuildError, test._build)
+
+
+class XCToolTestRun(unittest.TestCase):
+    def test_run_if_build_fails(self):
+        test = XCToolTest(None, None, None, None)
+        test._build = MagicMock(side_effect=BuildError)
+        self.assertRaises(BuildError, test.run)
+
+    def test_run_success(self):
+        test = XCToolTest(None, None, None, None)
+        test._execute = MagicMock(return_value=('', '', 0))
+        mock_parser = MagicMock()
+        dummy_tests = ['dummy', 'test', 'results']
+        mock_parser.tests = dummy_tests
+        test._get_parser = MagicMock(return_value=mock_parser)
+        self.assertEqual(dummy_tests, test.run())
+
+    def test_run_failure(self):
+        test = XCToolTest(None, None, None, None)
+        test._execute = MagicMock(return_value=('', '', 1))
+        test._build = MagicMock()
+        self.assertRaises(RunError, test.run)
+
--- xctool_test.py
+++ xctool_test.py
@@ -1,17 +1,32 @@
 import logging
 import subprocess
+from xctool_parser import XCToolParser
 
 logger = logging.getLogger(__name__)
 
 
 class XCToolError(Exception):
+    """generic XCTool failure"""
+    pass
+
+
+class BuildError(XCToolError):
+    """unable to build and hence can't execute tests"""
+    pass
+
+
+class RunError(XCToolError):
+    """unable to run tests for whatever reason"""
     pass
 
 
 class XCToolTest(object):
+    parser_class = XCToolParser
     default_xc_tool_path = '/Users/mtford/Scripts/XCTool/build/Products/Release/xctool'
     default_build_log_stdout = '/tmp/frodo-build.stdout.log'
     default_build_log_stderr = '/tmp/frodo-build.stderr.log'
+    default_run_log_stdout = '/tmp/frodo-run.stdout.log'
+    default_run_log_stderr = '/tmp/frodo-run.stderr.log'
     build_tests_cmd = '{xctool_path} ' \
                       '-workspace {workspace} ' \
                       '-scheme {scheme} ' \
@@ -23,11 +38,12 @@ class XCToolTest(object):
                'run-tests -only "{only}" ' \
                '-sdk {sdk} -reporter json-stream'
 
-    def __init__(self, workspace, scheme, target, test_class=None, test_method=None, xctool_path=None):
+    def __init__(self, workspace, scheme, sdk, target, test_class=None, test_method=None, xctool_path=None):
         super(XCToolTest, self).__init__()
         self._xctool_path = xctool_path
         self.workspace = workspace
         self.scheme = scheme
+        self.sdk = sdk
         self.target = target
         self.test_class = test_class
         self.test_method = test_method
@@ -43,6 +59,14 @@ class XCToolTest(object):
     @property
     def log_path_build_stderr(self):
         return self.default_build_log_stderr
+
+    @property
+    def log_path_run_stdout(self):
+        return self.default_run_log_stdout
+
+    @property
+    def log_path_run_stderr(self):
+        return self.default_run_log_stderr
 
     def _construct_only(self):
         only = self.target
@@ -77,43 +101,43 @@ class XCToolTest(object):
         if return_code:
             logger.fatal('Build failed with exit code %d. See %s and %s for more details.' % (
                 return_code, stdout_log, stderr_log))
+            raise BuildError()
         else:
             logger.info('Build successful')
-        success = not return_code
-        return success
 
-            # def run(self):
-            #     env = self._kwargs.get('env', None)
-            #     if env:
-            #         env = env.as_dict()
-            #     Logger.info("Running test '%s'" % self.name)
-            #     wd = self.configuration.working_dir
-            #     Logger.info("Changing to directory: '%s'" % wd)
-            #     os.chdir(wd)
-            #     self._build()
-            #     bash_cmd = self.bash_cmd.format(xctool=self.default_xc_tool,
-            #                                     workspace=self.config.workspace,
-            #                                     scheme=self.config.scheme,
-            #                                     only=self._construct_only(),
-            #                                     sdk=self.config.sdk)
-            #     Logger.debug("Executing '%s'" % bash_cmd)
-            #     process = subprocess.Popen(bash_cmd,
-            #                                stdout=subprocess.PIPE,
-            #                                stderr=subprocess.PIPE,
-            #                                env=env,
-            #                                shell=True)
-            #     stdout, stderr = process.communicate()
-            #     return_code = process.returncode
-            #     stdout_log = '/tmp/frodo-run.stdout.log'
-            #     stderr_log = '/tmp/frodo-run.stderr.log'
-            #     Logger.info('Writing %s' % stdout_log)
-            #     with open(stdout_log, 'w') as f:
-            #         f.write(stdout)
-            #     Logger.info('Writing %s' % stderr_log)
-            #     with open(stderr_log, 'w') as f:
-            #         f.write(stderr)
-            #     if not return_code in [0, 1]:
-            #         Logger.fatal('Test failed. See logs for more info.')
-            #     else:
-            #         Logger.info('Tests ran successfully.')
+    def _get_parser_class(self):
+        return self.parser_class
+
+    def _get_parser(self, stream):
+        parser_class = self._get_parser_class()
+        return parser_class(stream)
+
+    def _parse(self, stdout):
+        parser = self._get_parser(stdout.split('\n'))
+        parser.parse()
+        return parser.tests
+
+    def run(self):
+        self._build()
+        bash_cmd = self.bash_cmd.format(xctool_path=self.xctool_path,
+                                        workspace=self.workspace,
+                                        scheme=self.scheme,
+                                        only=self._construct_only(),
+                                        sdk=self.sdk)
+        stdout, stderr, return_code = self._execute(bash_cmd)
+        stdout_log = self.default_run_log_stdout
+        stderr_log = self.default_build_log_stdout
+        logger.info('Writing %s' % stdout_log)
+        with open(stdout_log, 'w') as f:
+            f.write(stdout)
+        logger.info('Writing %s' % stderr_log)
+        with open(stderr_log, 'w') as f:
+            f.write(stderr)
+        if return_code:
+            logger.fatal('Testing failed with exit code %d. See %s and %s for more details.' % (
+                return_code, stdout_log, stderr_log))
+            raise RunError()
+        else:
+            logger.info('Tests ran successfully.')
+            return self._parse(stdout)
 
